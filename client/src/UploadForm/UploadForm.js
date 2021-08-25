c9f76cb70b95e91c410850b0a2df2ee1ba7ef02e
Mumbai-123 <zealgodaddy@gmail.com>
2021-08-25 13:53:42 +0530

    Update UploadForm.js

--- client/src/UploadForm/UploadForm.js
+++ client/src/UploadForm/UploadForm.js
@@ -4,13 +4,54 @@ import { AppBar, Grid, Container } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
 import CustomAlert from "../components/CustomAlert/CustomAlert";
 import CustomBackdrop from "../components/CustomBackdrop/CustomBackdrop";
 
 const UploadForm = () => {
 	const classes = useStyles();
 
+	const [allClasses, setallClasses] = useState([]);
+	const [allSubjects, setallSubjects] = useState([]);
+	const [allChapters, setallChapters] = useState([]);
+	const [allTopics, setallTopics] = useState([]);
+
+	const [createClass, setcreateClass] = useState(false);
+	const [createSubject, setcreateSubject] = useState(false);
+	const [createChapter, setcreateChapter] = useState(false);
+	const [createTopic, setcreateTopic] = useState(false);
+
+	const [selectClass, setselectClass] = useState(true);
+	const [selectSubject, setselectSubject] = useState(true);
+	const [selectChapter, setselectChapter] = useState(true);
+	const [selectTopic, setselectTopic] = useState(true);
+
+	useEffect(() => {
+		async function fetch() {
+			const response = await axios.get(
+				"https://zealacademy.herokuapp.com/allClasses"
+			);
+
+			await setallClasses(response.data);
+		}
+
+		fetch();
+	}, []);
+
 	const [query, setQuery] = useState({
+		className: "",
+		subjectName: "",
+		chapterName: "",
+		topicName: "",
+		video: "",
+		pdf: "",
+		cI: 0,
+		sI: 0,
+		chI: 0,
+		tI: 0,
+	});
+
+	const [existing, setExisting] = useState({
 		className: "",
 		subjectName: "",
 		chapterName: "",
@@ -28,14 +69,15 @@ const UploadForm = () => {
 
 	let finalRes;
 	const upload = async () => {
+		console.log(existing);
 		setbackdrop(true);
 		if (
-			!query.className ||
-			!query.subjectName ||
-			!query.chapterName ||
-			!query.topicName ||
-			!query.video ||
-			!query.pdf
+			!existing.className ||
+			!existing.subjectName ||
+			!existing.chapterName ||
+			!existing.topicName ||
+			!existing.video ||
+			!existing.pdf
 		) {
 			seterrMsg("Please fill all required fields");
 			setbackdrop(false);
@@ -44,7 +86,7 @@ const UploadForm = () => {
 		}
 
 		try {
-			await axios.post("https://zealacademy.herokuapp.com/upload", query);
+			await axios.post("https://zealacademy.herokuapp.com/upload", existing);
 		} catch (error) {
 			finalRes = error.response.data;
 		}
@@ -52,6 +94,8 @@ const UploadForm = () => {
 		if (!finalRes) {
 			setsuccMsg("Successfully uploaded");
 			setsuccAlert(true);
+
+			window.location.reload();
 		} else {
 			seterrMsg(finalRes);
 			seterrAlert(true);
@@ -82,72 +126,279 @@ const UploadForm = () => {
 								}
 							}}
 						>
-							<TextField
-								label="Class"
-								style={{ margin: "10px 20px" }}
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								value={query.className}
-								onChange={(event) => {
-									setQuery({
-										...query,
-										className: event.target.value,
-									});
-								}}
-							/>
-							<TextField
-								label="Subject"
-								style={{ margin: "10px 20px" }}
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								value={query.subjectName}
-								onChange={(event) => {
-									setQuery({
-										...query,
-										subjectName: event.target.value,
-									});
-								}}
-							/>
-							<TextField
-								label="Chapter"
-								style={{ margin: "10px 20px" }}
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								value={query.chapterName}
-								onChange={(event) => {
-									setQuery({
-										...query,
-										chapterName: event.target.value,
-									});
-								}}
-							/>
-							<TextField
-								label="Topic"
-								style={{ margin: "10px 20px" }}
-								fullWidth
-								margin="normal"
-								variant="outlined"
-								value={query.topicName}
-								onChange={(event) => {
-									setQuery({
-										...query,
-										topicName: event.target.value,
-									});
-								}}
-							/>
+							{selectClass && (
+								<TextField
+									select
+									label="Select class"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={query.className}
+									onChange={(event) => {
+										if (event.target.value === "Create new") {
+											setcreateClass(true);
+											setcreateSubject(true);
+											setcreateChapter(true);
+											setcreateTopic(true);
+											setselectSubject(false);
+											setselectChapter(false);
+											setselectTopic(false);
+
+											setExisting({
+												...existing,
+												className: "",
+											});
+										} else {
+											setcreateClass(false);
+											setcreateSubject(false);
+											setcreateChapter(false);
+											setcreateTopic(false);
+											setselectSubject(true);
+											setselectChapter(true);
+											setselectTopic(true);
+
+											setQuery({ ...query, cI: event.target.value });
+											setallSubjects(allClasses[query.cI].subjects);
+											setExisting({
+												...existing,
+												className: allClasses[query.cI].name,
+											});
+										}
+										setQuery({
+											...query,
+											className: event.target.value,
+										});
+									}}
+								>
+									<MenuItem value="Create new">Create new</MenuItem>
+									{allClasses.map((item, key) => (
+										<MenuItem key={key} value={key}>
+											{item.name}
+										</MenuItem>
+									))}
+								</TextField>
+							)}
+							{createClass && (
+								<TextField
+									label="Enter class"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={existing.className}
+									onChange={(event) => {
+										setExisting({
+											...existing,
+											className: event.target.value,
+										});
+									}}
+								/>
+							)}
+							{selectSubject && (
+								<TextField
+									select
+									label="Select subject"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={query.subjectName}
+									onChange={(event) => {
+										if (event.target.value === "Create new") {
+											setcreateSubject(true);
+											setcreateChapter(true);
+											setcreateTopic(true);
+											setselectChapter(false);
+											setselectTopic(false);
+
+											setExisting({
+												...existing,
+												subjectName: "",
+											});
+										} else {
+											setcreateSubject(false);
+											setcreateChapter(false);
+											setcreateTopic(false);
+											setselectChapter(true);
+											setselectTopic(true);
+
+											setQuery({ ...query, sI: event.target.value });
+											setallChapters(
+												allClasses[query.cI].subjects[query.sI].chapters
+											);
+											setExisting({
+												...existing,
+												subjectName:
+													allClasses[query.cI].subjects[query.sI].subjectName,
+											});
+										}
+										setQuery({
+											...query,
+											subjectName: event.target.value,
+										});
+									}}
+								>
+									<MenuItem value="Create new">Create new</MenuItem>
+									{allSubjects.map((item, key) => (
+										<MenuItem key={key} value={key}>
+											{item.subjectName}
+										</MenuItem>
+									))}
+								</TextField>
+							)}
+							{createSubject && (
+								<TextField
+									label="Subject"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={existing.subjectName}
+									onChange={(event) => {
+										setExisting({
+											...existing,
+											subjectName: event.target.value,
+										});
+									}}
+								/>
+							)}
+							{selectChapter && (
+								<TextField
+									select
+									label="Select chapter"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={query.chapterName}
+									onChange={(event) => {
+										if (event.target.value === "Create new") {
+											setcreateChapter(true);
+											setcreateTopic(true);
+											setselectTopic(false);
+
+											setExisting({
+												...existing,
+												chapterName: "",
+											});
+										} else {
+											setcreateChapter(false);
+											setcreateTopic(false);
+											setselectTopic(true);
+
+											setQuery({ ...query, chI: event.target.value });
+											setallTopics(
+												allClasses[query.cI].subjects[query.sI].chapters[
+													query.chI
+												].topics
+											);
+											setExisting({
+												...existing,
+												chapterName:
+													allClasses[query.cI].subjects[query.sI].chapters[
+														query.chI
+													].chapterName,
+											});
+										}
+										setQuery({
+											...query,
+											chapterName: event.target.value,
+										});
+									}}
+								>
+									<MenuItem value="Create new">Create new</MenuItem>
+									{allChapters.map((item, key) => (
+										<MenuItem key={key} value={key}>
+											{item.chapterName}
+										</MenuItem>
+									))}
+								</TextField>
+							)}
+							{createChapter && (
+								<TextField
+									label="Chapter"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={existing.chapterName}
+									onChange={(event) => {
+										setExisting({
+											...existing,
+											chapterName: event.target.value,
+										});
+									}}
+								/>
+							)}
+							{selectTopic && (
+								<TextField
+									select
+									label="Select topic"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={query.topicName}
+									onChange={(event) => {
+										if (event.target.value === "Create new") {
+											setcreateTopic(true);
+
+											setExisting({
+												...existing,
+												topicName: "",
+											});
+										} else {
+											setcreateTopic(false);
+											setQuery({ ...query, tI: event.target.value });
+											setExisting({
+												...existing,
+												topicName:
+													allClasses[query.cI].subjects[query.sI].chapters[
+														query.chI
+													].topics[query.tI].topicName,
+											});
+										}
+										setQuery({
+											...query,
+											topicName: event.target.value,
+										});
+									}}
+								>
+									<MenuItem value="Create new">Create new</MenuItem>
+									{allTopics.map((item, key) => (
+										<MenuItem key={key} value={key}>
+											{item.topicName}
+										</MenuItem>
+									))}
+								</TextField>
+							)}
+							{createTopic && (
+								<TextField
+									label="Topic"
+									style={{ margin: "10px 20px" }}
+									fullWidth
+									margin="normal"
+									variant="outlined"
+									value={existing.topicName}
+									onChange={(event) => {
+										setExisting({
+											...existing,
+											topicName: event.target.value,
+										});
+									}}
+								/>
+							)}
 							<TextField
 								label="Video link"
 								style={{ margin: "10px 20px" }}
 								margin="normal"
 								variant="outlined"
 								fullWidth
-								value={query.video}
+								value={existing.video}
 								onChange={(event) => {
-									setQuery({
-										...query,
+									setExisting({
+										...existing,
 										video: event.target.value,
 									});
 								}}
@@ -158,10 +409,10 @@ const UploadForm = () => {
 								style={{ margin: "10px 20px" }}
 								margin="normal"
 								variant="outlined"
-								value={query.pdf}
+								value={existing.pdf}
 								onChange={(event) => {
-									setQuery({
-										...query,
+									setExisting({
+										...existing,
 										pdf: event.target.value,
 									});
 								}}
